34daf423a580a622459f16dbf49b407ba0d11f10
taehunkim <kamui178@naver.com>
2019-10-02 16:19:04 +0900

    test

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,2 +1,4 @@
-insert into posts(title, author, content, create_data, modified_data) values ('test1', 'test1@gamil.com', 'test1_content', now(), now());
-insert into posts(title, author, content, create_data, modified_data) values ('test2', 'test2@gamil.com', 'test2_content', now(), now());
+insert into posts(title, author, content, create_data, modified_data) 
+values ('test1', 'test1@gamil.com', 'test1_content', now(), now());
+insert into posts(title, author, content, create_data, modified_data) 
+values ('test2', 'test2@gamil.com', 'test2_content', now(), now());
