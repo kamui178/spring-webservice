b3fa422f6bfafb10fd79ccfc2193f7a113b61c86
taehunkim <kamui178@naver.com>
2019-10-18 10:30:54 +0900

    test

--- src/main/java/com/hoya/webservice/Application.java
+++ src/main/java/com/hoya/webservice/Application.java
@@ -3,6 +3,7 @@ package com.hoya.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -10,8 +11,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class Application {
+	
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/spring-webservice/real-application.yml";
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		new SpringApplicationBuilder(Application.class)
+		.properties(APPLICATION_LOCATIONS)
+		.run(args);
 	}
 }
---
+++ src/test/java/com/hoya/webservice/web/WebRestControllerTest.java
@@ -0,0 +1,13 @@
+package com.hoya.webservice.web;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class WebRestControllerTest {
+
+}
