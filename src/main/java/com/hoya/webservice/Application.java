49bb0b87174000452df28c74c6874ee06dd12a80
taehunkim <kamui178@naver.com>
2019-10-23 09:39:55 +0900

    test

--- src/main/java/com/hoya/webservice/Application.java
+++ src/main/java/com/hoya/webservice/Application.java
@@ -12,13 +12,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
 	
-//	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-//			+ "classpath:application.yml, "
-//			+ "/app/config/spring-webservice/real-application.yml";
-	
 	public static final String APPLICATION_LOCATIONS = "spring.config.location="
-			+ "classpath:application.yml";
-//			+ "classpath:/app/config/spring-webservice/real-application.yml";
+			+ "classpath:application.yml,"
+			+ "classpath:/app/config/spring-webservice/real-application.yml";
 
 
 	public static void main(String[] args) {
