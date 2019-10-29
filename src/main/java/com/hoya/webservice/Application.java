7cc3b98d2622cfe85af3accc1436295b7bcf18ff
kamui178@naver.com <qw848652>
2019-10-29 20:47:10 +0900

    test

--- src/main/java/com/hoya/webservice/Application.java
+++ src/main/java/com/hoya/webservice/Application.java
@@ -22,5 +22,6 @@ public class Application {
 		.properties(APPLICATION_LOCATIONS)
 		.run(args);
 	}
+
 }
 
--- src/main/java/com/hoya/webservice/web/WebRestController.java
+++ src/main/java/com/hoya/webservice/web/WebRestController.java
@@ -39,6 +39,7 @@ public class WebRestController {
 				.findFirst()
 				.orElse("");
 	}
+	
 
 	
 	
