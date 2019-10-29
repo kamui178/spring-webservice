2f8a41955a0feea24494692c6f8162510d8ccc6e
kamui178@naver.com <qw848652>
2019-10-29 22:43:45 +0900

    test

--- bin/main/data-h2.sql
+++ bin/main/data-h2.sql
@@ -1,4 +1,4 @@
 insert into posts(title, author, content, created_date, modified_date) 
-values ('test1', 'test1@gmail.com', 'test1_content', now(), now());
+values ('test1', 'test1@gmail.com', '', now(), now());
 insert into posts(title, author, content, created_date, modified_date) 
-values ('test2', 'test2@gamil.com', 'test2_content', now(), now());
+values ('test2', 'test2@gamil.com', '', now(), now());
